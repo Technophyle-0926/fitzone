ac836643d4bf37586e7eb276ddb065fd3be9423a
Technophyle-0926 <meet15107@gmail.com>
2025-01-11 19:33:16 +0530

    ✨feat: add search and filter functionality to food list view

--- lib/Controller/food_list_controller.dart
+++ lib/Controller/food_list_controller.dart
@@ -7,12 +7,23 @@ import 'package:http/http.dart' as http;
 class FoodListController extends GetxController {
   List<FoodList>? _foodlist;
   List<FoodList>? get foodlist => _foodlist;
+  List<FoodList>? _filteredfoodlist;
+  List<FoodList>? get filteredfoodlist => _filteredfoodlist;
   void getFoodList() async {
     var response = await http
         .get(Uri.parse("https://appy.trycatchtech.com/v3/fit_zone/food_list"));
     if (response.statusCode == 200) {
-      _foodlist = FoodList.ofFoodList(jsonDecode(response.body));
+      _foodlist =
+          _filteredfoodlist = FoodList.ofFoodList(jsonDecode(response.body));
       update();
     }
+  }
+
+  void filteredFoodList(value) {
+    _filteredfoodlist = foodlist
+        ?.where(
+            (food) => food.name!.isCaseInsensitiveContainsAny(value.toString()))
+        .toList();
+    update();
   }
 }
--- lib/View/food_view.dart
+++ lib/View/food_view.dart
@@ -28,82 +28,141 @@ class _FoodViewState extends State<FoodView> {
         title: Text("Featured Food"),
         centerTitle: true,
       ),
-      body: GetBuilder<FoodListController>(
-        builder: (foodListController) {
-          return foodListController.foodlist == null
-              ? Center(
-                  child: CircularProgressIndicator.adaptive(),
-                )
-              : Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: GridView.builder(
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      mainAxisSpacing: 10,
-                      crossAxisSpacing: 25,
-                      childAspectRatio: .7,
-                    ),
-                    itemCount: foodListController.foodlist!.length,
-                    itemBuilder: (context, i) {
-                      return GestureDetector(
-                        onTap: () {
-                          Get.to(
-                            () => FoodDetailsView(),
-                            arguments: {
-                              "id": "${foodListController.foodlist![i].id}",
-                              "name": "${foodListController.foodlist![i].name}"
-                            },
-                          );
-                        },
-                        child: Card(
-                          color: Colors.white,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                ClipRRect(
-                                  borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(10),
-                                    topRight: Radius.circular(10),
-                                  ),
-                                  child: Image.network(
-                                    foodListController.foodlist![i].image ?? '',
-                                  ),
-                                ),
-                                Text(
-                                  foodListController.foodlist![i].name ?? '',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 20,
-                                  ),
-                                ),
-                                Text(
-                                  "Calories: ${foodListController.foodlist![i].calories ?? ''} Kcal",
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.w400,
-                                    fontSize: 18,
-                                    color: Colors.grey,
-                                  ),
-                                ),
-                                Text(
-                                  "100 gm",
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.w400,
-                                    fontSize: 15,
-                                    color: Colors.grey,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      );
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  width: 310,
+                  child: SearchBar(
+                    onSubmitted: (value) =>
+                        foodListController.filteredFoodList(value),
+                    onChanged: (event) {
+                      foodListController.filteredfoodlist ==
+                          foodListController.foodlist;
                     },
+                    hintText: "Type here to search something",
                   ),
-                );
-        },
+                ),
+                InkWell(
+                  onTap: () {
+                    Get.defaultDialog(
+                      title: "Filter",
+                    );
+                  },
+                  child: Container(
+                    height: 55,
+                    width: 55,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey.shade200,
+                    ),
+                    child: Icon(
+                      Icons.filter_alt_outlined,
+                      size: 50,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: GetBuilder<FoodListController>(
+                builder: (foodListController) {
+                  return foodListController.filteredfoodlist == null
+                      ? Center(
+                          child: CircularProgressIndicator.adaptive(),
+                        )
+                      : Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                          child: GridView.builder(
+                            gridDelegate:
+                                SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                              mainAxisSpacing: 10,
+                              crossAxisSpacing: 25,
+                              childAspectRatio: .7,
+                            ),
+                            itemCount:
+                                foodListController.filteredfoodlist!.length,
+                            itemBuilder: (context, i) {
+                              return GestureDetector(
+                                onTap: () {
+                                  Get.to(
+                                    () => FoodDetailsView(),
+                                    arguments: {
+                                      "id":
+                                          "${foodListController.filteredfoodlist![i].id}",
+                                      "name":
+                                          "${foodListController.filteredfoodlist![i].name}"
+                                    },
+                                  );
+                                },
+                                child: Card(
+                                  elevation: 10,
+                                  color: Colors.white,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: [
+                                        ClipRRect(
+                                          borderRadius: BorderRadius.only(
+                                            topLeft: Radius.circular(10),
+                                            topRight: Radius.circular(10),
+                                          ),
+                                          child: Image.network(
+                                            foodListController
+                                                    .filteredfoodlist![i]
+                                                    .image ??
+                                                '',
+                                          ),
+                                        ),
+                                        Text(
+                                          foodListController
+                                                  .filteredfoodlist![i].name ??
+                                              '',
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 20,
+                                          ),
+                                        ),
+                                        Text(
+                                          "Calories: ${foodListController.filteredfoodlist![i].calories ?? ''} Kcal",
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.w400,
+                                            fontSize: 18,
+                                            color: Colors.grey,
+                                          ),
+                                        ),
+                                        Text(
+                                          "100 gm",
+                                          style: TextStyle(
+                                            fontWeight: FontWeight.w400,
+                                            fontSize: 15,
+                                            color: Colors.grey,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        );
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
